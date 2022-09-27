045cdf16ac6fe7fb062d097df79109d820595e27
Philip Gideon <gphilip240@gmail.com>
2022-09-27 14:55:37 -0700

    0x07- Pointers

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -1,25 +1,48 @@
-#ifndef MAIN_H
-#define MAIN_H
+#ifndef MAIN_H                                                                                                                     
 
-int _putchar(char c);
+#define MAIN_H                                                                                                                     
 
-char *_memset(char *s, char b, unsigned int n);
+                                                                                                                                   
 
-char *_memcpy(char *dest, char *src, unsigned int n);
+/**                                                                                                                               
 
-char *_strchr(char *s, char c);
+ * File: main.h                                                                                                                   
 
-unsigned int _strspn(char *s, char *accept);
+ * Auth: sam tech (samuel birhanu)                                                                                                 
 
-char *_strpbrk(char *s, char *accept);
+ * description: Header file containing prototypes for all functions                                                               
 
-char *_strstr(char *haystack, char *needle);
+ *       used in the 0x06-pointers_arrays_strings directory.                                                                       
 
-void print_chessboard(char (*a)[8]);
+ */                                                                                                                               
 
-void print_diagsums(int *a, int size);
+                                                                                                                                   
 
-void set_string(char **s, char *to);
+#include <stddef.h>                                                                                                               
 
+                                                                                                                                   
+
+int _putchar(char c);                                                                                                              
+
+void *_memset(void *s, int c, size_t n);                                                                                          
+
+char *_memcpy(char *dest, char *src, unsigned int n);                                                                              
+
+char *_strchr(char *s, char c);                                                                                                    
+
+unsigned int _strspn(char *s, char *accept);                                                                                      
+
+char *_strpbrk(char *s, char *accept);                                                                                            
+
+char *_strstr(char *haystack, char *needle);                                                                                      
+
+void print_chessboard(char (*a)[8]);                                                                                              
+
+void print_diagsums(int *a, int size);                                                                                            
+
+void set_string(char **s, char *to);                                                                                              
+
+                                                                                                                                   
 
 #endif
+
